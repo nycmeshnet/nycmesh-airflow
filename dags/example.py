7237e5872967f429766a630a3427af09c62f89bc
james-otten <jamesotten1@gmail.com>
2025-06-15 15:12:22 -0400

    init

--- dags/example.py
+++ dags/example.py
@@ -9,7 +9,7 @@ from airflow.utils.dates import days_ago
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.operators.kubernetes_job_operator import KubernetesJobOperator
 
 args = {
     'owner': 'Airflow',
@@ -61,7 +61,7 @@ for i in range(5):
 
     run_this >> task
 
-k = KubernetesPodOperator(namespace='default',
+k = KubernetesJobOperator(namespace='default',
                           image="ubuntu:20.04",
                           cmds=["bash", "-cx"],
                           arguments=["echo", "Hello from k8s"],

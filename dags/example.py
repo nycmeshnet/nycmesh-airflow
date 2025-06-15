f83708d4a64014b5bdf396b301b181478c6ba942
james-otten <jamesotten1@gmail.com>
2025-06-15 14:53:32 -0400

    init

---
+++ dags/example.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2),
+}
+
+dag = DAG(
+    dag_id='example_python_operator',
+    default_args=args,
+    schedule_interval=None,
+    tags=['example']
+)
+
+
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag,
+)
+
+also_run_this = BashOperator(
+    task_id='also_run_this',
+    bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
+    dag=dag,
+)
+
+also_run_this >> run_this
+
+def my_sleeping_function(random_base):
+    """This is a function that will run within the DAG execution"""
+    time.sleep(random_base)
+
+
+# Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
+for i in range(5):
+    task = PythonOperator(
+        task_id='sleep_for_' + str(i),
+        python_callable=my_sleeping_function,
+        op_kwargs={'random_base': float(i) / 10},
+        dag=dag,
+    )
+
+    run_this >> task
+
+k = KubernetesPodOperator(namespace='default',
+                          image="ubuntu:20.04",
+                          cmds=["bash", "-cx"],
+                          arguments=["echo", "Hello from k8s"],
+                          name="test",
+                          task_id="task",
+                          hostnetwork=False,
+)
